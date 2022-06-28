a548ecf1c7b14cf015ba6d10f29ee20b105bb1f8
Hector Norry <piscudelag@gmail.com>
2022-06-28 10:56:07 -0300

    Se agrega punto 2 al fin

--- js/app.js
+++ js/app.js
@@ -1,3 +1,5 @@
+let personas = [];
+
 class Persona {
   constructor(nombre, edad, dni, sexo, peso, altura, nacimiento) {
     this.nombre = nombre;
@@ -9,66 +11,75 @@ class Persona {
     this.nacimiento = nacimiento;
   }
 
-  mayoriaEdad() {
-    if (this.edad >= 18) {
-      document.write(`${this.nombre} es mayor de edad.<br>`);
-    }
+  
+
+}
+
+function mayoriaEdad() {
+  if (this.edad >= 18) {
+    alert(`${this.nombre} es mayor de edad.`);
   }
+}
 
-  mostrarDatos() {
-    document.write(`
-        <ul>
-        <li>Nombre: ${this.nombre}</li>
-        <li>Edad: ${this.edad}</li>
-        <li>DNI: ${this.dni}</li>
-        <li>Sexo: ${this.sexo}</li>
-        <li>Peso: ${this.peso}</li>
-        <li>Altura: ${this.altura}</li>
-        <li>Año de nacimiento: ${this.nacimiento}</li>
-        </ul>
-        `);
-  }
+function mostrarDatos() {
+  alert(`
+      
+      Nombre: ${this.nombre}
+      Edad: ${this.edad} años
+      DNI: ${this.dni}
+      Sexo: ${this.sexo}
+      Peso: ${this.peso}
+      Altura: ${this.altura}
+      Año de nacimiento: ${this.nacimiento}
+      
+      `);
+}
 
-  mostrarGeneracion() {
-    if (this.nacimiento >= 1930 && this.nacimiento <= 1948) {
-      alert(
-        `${this.nombre} pertecene a la generacion Silent Generation<br>Su rasgo caracteristico: Austeridad.<br>`
-      );
-    }
-
-    if (this.nacimiento >= 1949 && this.nacimiento <= 1968) {
-      alert(
-        `${this.nombre} pertenece a la generacion Baby boom<br>Su rasgo caracteristico: Ambicion.<br>`
-      );
-    }
-    if (this.nacimiento >= 1969 && this.nacimiento <= 1980) {
-      alert(
-        `${this.nombre} pertenece a la generacion X<br>Su rasgo caracteristico: Obsesion por el exito.<br>`
-      );
-    }
-    if (this.nacimiento >= 1981 && this.nacimiento <= 1993) {
-      alert(
-        `${this.nombre} pertenece a la generacion Y<br>Rasgo caracteristico: Frustracion.<br>`
-      );
-    }
-    if (this.nacimiento >= 1994 && this.nacimiento <= 2010) {
-      alert(
-        `${this.nombre} pertenece a la generacion Z<br>Rasgo caracteristico: irreverencia.<br>`
-      );
-    }
-  }
-
-  crearPersona() {
-    const persona = new Persona(
-      (nombre = document.getElementById("#Fnombre")),
-      (edad = document.querySelector("#Fedad")),
-      (dni = document.querySelector("#Fdni")),
-      (sexo = document.querySelector("#Fsexo")),
-      (peso = document.querySelector("#Fpeso")),
-      (altura = document.querySelector("#Faltura")),
-      (nacimiento = document.querySelector("#Fnacimiento"))
+function mostrarGeneracion() {
+  if (this.nacimiento >= 1930 && this.nacimiento <= 1948) {
+    alert(
+      `${this.nombre} pertecene a la generacion Silent Generation. Su rasgo caracteristico: Austeridad.`
     );
-   
-    console.log(this.Persona);
   }
+
+  if (this.nacimiento >= 1949 && this.nacimiento <= 1968) {
+    alert(
+      `${this.nombre} pertenece a la generacion Baby boom. Su rasgo caracteristico: Ambicion.`
+    );
+  }
+  if (this.nacimiento >= 1969 && this.nacimiento <= 1980) {
+    alert(
+      `${this.nombre} pertenece a la generacion X. Su rasgo caracteristico: Obsesion por el exito.`
+    );
+  }
+  if (this.nacimiento >= 1981 && this.nacimiento <= 1993) {
+    alert(
+      `${this.nombre} pertenece a la generacion Y. Su rasgo caracteristico: Frustracion.`
+    );
+  }
+  if (this.nacimiento >= 1994 && this.nacimiento <= 2010) {
+    alert(
+      `${this.nombre} pertenece a la generacion Z. Rasgo caracteristico: irreverencia.`
+    );
+  }
+}
+
+function crearPersona() {
+
+  
+  const persona = new Persona(
+    
+    nombre = document.getElementById('Fnombre').value,
+    edad = document.querySelector("#Fedad").value,
+    dni = document.querySelector("#Fdni").value,
+    sexo = document.querySelector("#Fsexo").value,
+    peso = document.querySelector("#Fpeso").value,
+    altura = document.querySelector("#Faltura").value,
+    nacimiento = document.querySelector("#Fnacimiento").value
+
+  );
+
+  personas.push(persona)
+  console.log(persona);
+  console.log(personas);
 }
